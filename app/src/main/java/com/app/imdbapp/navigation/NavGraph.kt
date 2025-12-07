43c422336d75d71d9001a6d5b21719d1a1b39327
Harsh <harshsingh071@gmail.com>
2025-12-08 00:26:37 +0530

    Navgraph setting is done

---
+++ app/src/main/java/com/app/imdbapp/navigation/NavGraph.kt
@@ -0,0 +1,46 @@
+package com.app.imdbapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.app.imdbapp.ui.screens.MovieDetailScreen
+import com.app.imdbapp.ui.screens.MovieListScreen
+
+sealed class Screen(val route: String) {
+    object MovieList : Screen("movie_list")
+    object MovieDetail : Screen("movie_detail/{movieId}") {
+        fun createRoute(movieId: Int) = "movie_detail/$movieId"
+    }
+}
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.MovieList.route
+    ) {
+        composable(Screen.MovieList.route) {
+            MovieListScreen(
+                onMovieClick = { movieId ->
+                    navController.navigate(Screen.MovieDetail.createRoute(movieId))
+                }
+            )
+        }
+
+        composable(
+            route = Screen.MovieDetail.route,
+            arguments = listOf(navArgument("movieId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val movieId = backStackEntry.arguments?.getInt("movieId") ?: 0
+            MovieDetailScreen(
+                movieId = movieId,
+                onNavigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
